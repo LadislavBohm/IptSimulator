83637843a4b04dcf6df5ff1520c08df0c50e03c2
Ladislav Böhm <bohmladislav@gmail.com>
2016-10-09 10:49:34 +0200

    refactoring
    move methods to Core extensions

--- IptSimulator.CiscoTcl/Commands/FsmDefine.cs
+++ IptSimulator.CiscoTcl/Commands/FsmDefine.cs
@@ -8,11 +8,14 @@ using Eagle._Components.Public;
 using Eagle._Containers.Public;
 using Eagle._Interfaces.Public;
 using IptSimulator.CiscoTcl.Commands.Abstractions;
+using IptSimulator.Core;
 
 namespace IptSimulator.CiscoTcl.Commands
 {
     public class FsmDefine : CiscoTclCommand
     {
+        private readonly IDictionary<string,string> _transitions = new Dictionary<string, string>();
+
         public FsmDefine() : base(
             new CommandData("fsm define", null, null, null, typeof(FsmDefine).FullName, CommandFlags.None, null, 0))
         {
@@ -34,7 +37,7 @@ namespace IptSimulator.CiscoTcl.Commands
             var fsmArray = arguments[2];
             var initialState = arguments[3];
 
-            if (!ArrayExists(interpreter, fsmArray))
+            if (!interpreter.ArrayExists(fsmArray))
             {
                 var arrayNotExists = $"Array of fsm states with name: {fsmArray} does not exist (is not defined).";
 
@@ -49,22 +52,7 @@ namespace IptSimulator.CiscoTcl.Commands
             return ReturnCode.Ok;
         }
 
-        private bool ArrayExists(Interpreter interpreter, string arrayName)
-        {
-            if (interpreter == null) throw new ArgumentNullException(nameof(interpreter));
-            if (string.IsNullOrEmpty(arrayName))
-                throw new ArgumentException("Value cannot be null or empty.", nameof(arrayName));
-
-            Result result = null;
-            ReturnCode code = interpreter.EvaluateScript($"array exists {arrayName}", ref result);
-
-            if (code != ReturnCode.Ok)
-            {
-                return false;
-            }
-
-            return string.Equals("1", result.String);
-        }
+        
 
         private bool ProcedureExists(Interpreter interpreter, string procedureName)
         {
