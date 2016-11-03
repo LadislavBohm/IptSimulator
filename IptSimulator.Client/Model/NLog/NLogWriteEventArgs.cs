2a8293af49aa88f6c6fe83d6d2cd48f9a8609736
Ladislav Böhm <bohmladislav@gmail.com>
2016-11-03 17:33:08 +0100

    basic debug window control

---
+++ IptSimulator.Client/Model/NLog/NLogWriteEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace IptSimulator.Client.Model.NLog
+{
+    public class NLogWriteEventArgs : EventArgs
+    {
+        public NLogWriteEventArgs(LogEventInfo logEventInfo)
+        {
+            LogEventInfo = logEventInfo;
+        }
+
+        public LogEventInfo LogEventInfo { get; }
+    }
+}
