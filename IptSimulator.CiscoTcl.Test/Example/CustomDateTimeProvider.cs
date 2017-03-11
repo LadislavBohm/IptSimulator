44d197ed0185694dd24a9c1baf28cdb0b5260a9d
Ladislav Böhm <bohmladislav@gmail.com>
2017-03-11 18:28:58 +0100

    add unit test example

---
+++ IptSimulator.CiscoTcl.Test/Example/CustomDateTimeProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace IptSimulator.CiscoTcl.Test.Example
+{
+    public class CustomDateTimeProvider: IDateTimeProvider
+    {
+        private readonly DateTime _customDate;
+
+        public CustomDateTimeProvider(DateTime customDate)
+        {
+            _customDate = customDate;
+        }
+
+        public DateTime GetDateTime() => _customDate;
+    }
+}
