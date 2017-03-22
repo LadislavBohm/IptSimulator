e693ba77009ca9ba7cbdf3ba22924730c43bec98
Ladislav Böhm <bohmladislav@gmail.com>
2017-03-22 12:21:51 +0100

    add static value subtractor

---
+++ IptSimulator.Client/Converters/DoubleMinusConstantConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace IptSimulator.Client.Converters
+{
+    public class DoubleMinusConstantConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var intValue = value as double?;
+            if (!intValue.HasValue)
+            {
+                return value;
+            }
+            var paramInt = parameter as int?;
+            if (!paramInt.HasValue)
+            {
+                return intValue.Value;
+            }
+
+            return intValue.Value - paramInt.Value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
