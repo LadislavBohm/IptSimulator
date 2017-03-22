037ab9e8f7b582cf240a0523f047da3b65c13df8
Ladislav Böhm <bohmladislav@gmail.com>
2017-03-22 17:18:18 +0100

    disable selection of single-value rows

--- IptSimulator.Client/Controls/Dockable/WatchWindow.xaml.cs
+++ IptSimulator.Client/Controls/Dockable/WatchWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+using IptSimulator.CiscoTcl.Model;
+using IptSimulator.Client.ViewModels.Data;
 
 namespace IptSimulator.Client.Controls.Dockable
 {
@@ -19,6 +22,22 @@ namespace IptSimulator.Client.Controls.Dockable
             if (row != null)
             {
                 TopVariableGrid.UnselectAll();
+            }
+        }
+
+        private void TopVariableGrid_RowSelectedChanged(object sender, RoutedEventArgs e)
+        {
+            var row = sender as DataGridRow;
+            var vm = row?.DataContext as WatchVariableViewModel;
+            if (vm == null)
+            {
+                return;
+            }
+
+            if (row.IsSelected && !vm.HasSubValues)
+            {
+                //disable selection of single value rows
+                row.IsSelected = false;
             }
         }
     }
