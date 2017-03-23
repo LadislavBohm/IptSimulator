8803075bed07bebc5ee653782d8c97f940c64084
Ladislav Böhm <bohmladislav@gmail.com>
2017-03-23 22:54:02 +0100

    allow delayed execution

--- IptSimulator.CiscoTcl/TclInterpreter/EventArgs/DebugModeEventArgs.cs
+++ IptSimulator.CiscoTcl/TclInterpreter/EventArgs/DebugModeEventArgs.cs
@@ -2,13 +2,15 @@
 {
     public class DebugModeEventArgs : System.EventArgs
     {
-        public DebugModeEventArgs(bool isBreakpointHit, int? lineNumber = null)
+        public DebugModeEventArgs(bool isBreakpointHit, bool isDelayedLineHit, int? lineNumber = null)
         {
             IsBreakpointHit = isBreakpointHit;
+            IsDelayedLineHit = isDelayedLineHit;
             LineNumber = lineNumber;
         }
 
         public bool IsBreakpointHit { get; private set; }
         public int? LineNumber { get; set; }
+        public bool IsDelayedLineHit { get; private set; }
     }
 }
